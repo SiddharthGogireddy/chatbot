2af2345785cf9119582d592e6116a82750dfa59e
GradientBooster <144033691+aditya0589@users.noreply.github.com>
2025-07-06 11:23:37 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+
 <!-- header.php -->
 <header>
     <nav class="navbar navbar-expand-lg">
@@ -8,14 +9,14 @@
             </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link" href="main.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
                     <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
                     <li class="nav-item"><a class="nav-link" href="industry_trends.php">Industry Trends</a></li>
-                    <li class="nav-item"><a class="nav-link" href="careers_list.php">View Careers</a></li>
-                    <li class="nav-item"><a class="nav-link" href="roadmap_input.php">Roadmap Generator</a></li>
-                    <?php if (isset($_SESSION['email'])): ?>
+                    <?php if (isset($_SESSION['user_id'])): ?>
                         <li class="nav-item"><a class="nav-link" href="alumni.php">Alumni</a></li>
+                        <li class="nav-item"><a class="nav-link" href="careers_list.php">View Careers</a></li>
+                        <li class="nav-item"><a class="nav-link" href="roadmap_input.php">Roadmap Generator</a></li>
                         <li class="nav-item">
                             <form action="logout.php" method="post" class="d-inline">
                                 <button type="submit" class="btn btn-logout">Logout</button>
